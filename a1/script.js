68779dc8589969c1e3a236aa0fbd5e884d62d98f
arick_yumol <arick.yumol@gmail.com>
2021-10-08 18:15:12 +0800

    Added get users and add user functionality

--- a1/script.js
+++ a1/script.js
@@ -1,47 +1,3 @@
-/*const getUser = () => {
-	fetch('https://jsonplaceholder.typicode.com/users')
-	.then( response => response.json())
-	.then( data => {
-		let users = '';
-		data.forEach(user => {
-			users =+ `
-				<div>
-					<h5>${user.name}</h5>
-					<p>${user.email}</p>
-					<p>${user.company}</p>
-					<p>${user.phone}</p>
-				</div>
-			`
-		});
-		document.querySelector('#output').innerHTML = users;
-	})
-	.catch( err => console.log('rejected'))
-}
-
-// addUser incomplete
-const addUser = (e) => {
-	e.preventDefault();
-
-	fetch('https://jsonplaceholder.typicode.com/users', {
-		method: 'POST',
-		headers: {
-			'Accept': 'application/json, text/plain',
-			'Content-type': 'application/json'
-		}
-		body: JSON.stringify({
-
-		})
-	})
-	.then( response => response.json())
-	.then( data => console.log(data))
-}
-
-const getUsers = document.querySelector('#getUsers');
-getUsers.addEventListener('click', getUser);
-
-const addUser = document.querySelector('#addUser');
-addUser.addEventListener('submit', addUser)*/
-
 const getUser = () => {
 	fetch('https://jsonplaceholder.typicode.com/users')
 	.then((response) => {
@@ -51,19 +7,73 @@ const getUser = () => {
 		data.forEach((user) => {
 			users += `
 				<div>
-					<h2>${user.name}</h2>
-					<p>${user.email}</p>
-					<p>${user.company.name}</p>
-					<p>${user.phone}</p>
+					<h2>Name: ${user.name}</h2>
+					<p>Email: ${user.email}</p>
+					<p>Company Name: ${user.company.name}</p>
+					<p>Phone: ${user.phone}</p>
 				</div>
 			`
 		});
 		document.querySelector('#output').innerHTML = users;
 	}).catch((err) => {
-		console.log('rejected')
+		console.log('rejected');
 	})
 }
+
+const addUser = (e) => {
+	e.preventDefault();
+
+	const userName = document.querySelector('#userName').value;
+	const userUsername = document.querySelector('#userUsername').value;
+	const userEmail = document.querySelector('#userEmail').value;
+	const userPhone = document.querySelector('#userPhone').value;
+	const userWebsite = document.querySelector('#userWebsite').value;
+	const useruserAddressStreet = document.querySelector('#userAddressStreet').value;
+	const userAddressSuite = document.querySelector('#userAddressSuite').value;
+	const userAddressCity = document.querySelector('#userAddressCity').value;
+	const userAddressZipcode = document.querySelector('#userAddressZipcode').value;
+	const userAddressGeoLatitude = document.querySelector('#userAddressGeoLatitude').value;
+	const userAddressGeoLongitude = document.querySelector('#userAddressGeoLongitude').value;
+	const userCompanyName = document.querySelector('#userCompanyName').value;
+	const userCompanyCatchphrase = document.querySelector('#userCompanyCatchphrase').value;
+	const userCompanybs = document.querySelector('#userCompanybs').value;
+
+	fetch('https://jsonplaceholder.typicode.com/users', {
+		method: 'POST',
+		headers: {
+			'Accept': 'application/json, text/plain',
+			'Content-type': 'application/json'
+		},
+		body: JSON.stringify({
+			name: userName,
+			username: userUsername,
+			email: userEmail,
+			address: {
+				street: useruserAddressStreet,
+				suite: userAddressSuite,
+				city: userAddressCity,
+				zipcode: userAddressZipcode,
+				geo: {
+					lat: userAddressGeoLatitude,
+					lng: userAddressGeoLongitude
+				}
+			},
+			phone: userPhone,
+			website: userWebsite,
+			company: {
+				name: userCompanyName,
+				catchphrase: userCompanyCatchphrase,
+				bs: userCompanybs
+			}
+		})
+	})
+	.then( response => response.json())
+	.then( data => console.log(data))
+}
+
 
 const getUsers = document.querySelector('#getUsers');
 getUsers.addEventListener('click', getUser);
 
+const addUsers = document.querySelector('#addUser');
+addUsers.addEventListener('submit', addUser);
